f1886549c8ecaf9061871d27d76315058c68c129
Kumetsa <i.e.ivanov1989@gmail.com>
2023-10-17 23:14:48 +0300

    Update settings.py

--- PythonORM/settings.py
+++ PythonORM/settings.py
@@ -75,12 +75,9 @@ WSGI_APPLICATION = 'PythonORM.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "OPTIONS": {
-            "service": "my_service",
-            "passfile": ".my_pgpass",
-        },
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
