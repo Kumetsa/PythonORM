cf6a1345cf33b1183081a74d9e586ab5d4419e0b
Kumetsa <i.e.ivanov1989@gmail.com>
2023-11-22 22:12:09 +0200

    Task 3

--- 19_Exercise Advanced Queries in Django/main_app/managers.py
+++ 19_Exercise Advanced Queries in Django/main_app/managers.py
@@ -1,7 +1,6 @@
 from decimal import Decimal
-
 from django.db import models
-from django.db.models import QuerySet, Count
+from django.db.models import QuerySet, Count, Max, Min, Avg
 
 
 class RealEstateListingManager(models.Manager):
@@ -17,4 +16,22 @@ class RealEstateListingManager(models.Manager):
     def popular_locations(self) -> QuerySet:
         return self.values('location').annotate(
             location_count=Count('location')
-        ).order_by('-location_count','id')[:2]
+        ).order_by('-location_count', 'id')[:2]
+
+
+class VideoGameManager(models.Manager):
+    def games_by_genre(self, genre: str) -> QuerySet:
+        return self.filter(genre=genre)
+
+    def recently_released_games(self, year: int) -> QuerySet:
+        return self.filter(release_year__gte=year)
+
+    def highest_rated_game(self):
+        return self.annotate(max_rating=Max('rating')).order_by('-max_rating').first()
+
+    def lowest_rated_game(self):
+        return self.annotate(min_rating=Min('rating')).order_by('min_rating').first()
+
+    def average_rating(self):
+        avg_rating = self.aggregate(avg_rating=Avg('rating'))["avg_rating"]
+        return f"{avg_rating:.1f}"
--- 19_Exercise Advanced Queries in Django/main_app/models.py
+++ 19_Exercise Advanced Queries in Django/main_app/models.py
@@ -1,6 +1,8 @@
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.db.models import QuerySet
 
-from main_app.managers import RealEstateListingManager
+from main_app.managers import RealEstateListingManager, VideoGameManager
 
 
 # Create your models here.
@@ -35,7 +37,20 @@ class VideoGame(models.Model):
     title = models.CharField(max_length=100)
     genre = models.CharField(max_length=100, choices=GENRE_CHOICES)
     release_year = models.PositiveIntegerField()
-    rating = models.DecimalField(max_digits=2,decimal_places=1)
+    rating = models.DecimalField(max_digits=2, decimal_places=1)
+
+    objects = VideoGameManager()
+
+    def clean(self):
+        if not 0.0 <= self.rating <= 10.0:
+            raise ValidationError("The rating must be between 0.0 and 10.0")
+
+        if not 1990 <= self.release_year <= 2023:
+            raise ValidationError("The release year must be between 1990 and 2023")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
@@ -48,6 +63,21 @@ class BillingInfo(models.Model):
 class Invoice(models.Model):
     invoice_number = models.CharField(max_length=20, unique=True)
     billing_info = models.OneToOneField(BillingInfo, on_delete=models.CASCADE)
+
+    @classmethod
+    def get_invoices_with_prefix(cls, prefix) -> QuerySet:
+        return cls.objects.select_related('billing_info').filter(invoice_number__startswith=prefix)
+
+    @classmethod
+    def get_invoices_sorted_by_number(cls) -> QuerySet:
+        return cls.objects.select_related('billing_info').order_by('invoice_number')
+
+    @classmethod
+    def get_invoice_with_billing_info(cls, invoice_number: str) -> object:
+        return cls.objects.select_related('billing_info').get(invoice_number=invoice_number)
+
+
+
 
 
 class Technology(models.Model):
